a841987acc0a40f22f14756dbf4566f0e03b15f6
Tarun-karrthick-rs <karrthicktarun@gamil.com>
2024-07-30 23:02:41 +0530

    basic_Java

---
+++ java.js
@@ -0,0 +1,11 @@
+var email="Tarun123@gamil.com";
+var pwd=123456;
+var userTypingemail="Tarun123@gamil.com";
+var userTypingpwd=123456;
+if(userTypingemail == email && userTypingpwd==pwd)
+{
+    console.log("Welcome");
+}
+else{
+    console.log("Email or Password is incorrect");
+}
